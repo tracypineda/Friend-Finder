ba77990aa4bce9fb83e681c7ea8c2aed25d97916
Tracy Pineda <tracypineda@gmail.com>
2019-10-14 12:52:07 -0600

    fixing the dropdown buttons

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = {
+    "name": "Tracy",
+    "photo": "https://www.pexels.com/photo/four-orange-jellyfish-wallpaper-1784578/",
+    "scoresArray": [
+        1,
+        3,
+        4,
+        2,
+        3,
+        1,
+        3,
+        1,
+        5,
+        3
+    ]
+}
+module.exports = friends;
