6a3013a074bf0c3d2cbbe0ee12463480bdd9e7ed
Tracy Pineda <tracypineda@gmail.com>
2019-10-13 22:17:23 -0600

    writing html pages

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-var express = require ("express");
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
 
+// Set up Express app
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+// Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/app/public'));
 
-
+// Import routes.
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
-
-
-
+// start listen
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+	console.log("App listening on PORT " + PORT);
 });
